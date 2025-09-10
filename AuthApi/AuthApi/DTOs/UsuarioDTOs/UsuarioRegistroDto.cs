cadec5bb0ed8ea9723e9c732faa9c0ad7ef87e89
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-09-10 04:50:12 -0600

    Codificar funcionalidades de API

---
+++ AuthApi/AuthApi/DTOs/UsuarioDTOs/UsuarioRegistroDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthApi.DTOs.UsuarioDTOs
+{
+    public class UsuarioRegistroDto
+    {
+        public string Nombre { get; set; } = "";
+
+        public string Email { get; set; } = "";
+
+        public string Password { get; set; } = "";
+    }
+}
---
+++ AuthApi/AuthApi/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using AuthApi.DTOs.UsuarioDTOs;
+
+namespace AuthApi.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<UsuarioRespuestaDto> RegistrarAsync(UsuarioRegistroDto dto);
+        Task<UsuarioRespuestaDto?> LoginAsync(UsuarioLoginDto dto);
+    }
+}
